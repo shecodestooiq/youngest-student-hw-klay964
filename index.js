e2c440a5b7797d3b11c7d16a3f63ba5a52c648df
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-02-12 13:11:44 +0300

    show youngest student

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(students) {
+  var youngest = students[0];
+  for (var i = 1; i < students.length; i++) {
+    if (students[i].age < youngest.age) {
+      youngest = students[i];
+    }
+  }
+  console.log(youngest.name);
+}
+var students = [
+  { name: 'John', age: 60 },
+  { name: 'Bill', age: 21 },
+  { name: 'Kate', age: 22 },
+];
+showYoungestStudent(students);
 
 module.exports = showYoungestStudent;
